1743a922ff06349e9e8a6afe306e12bf074cf6b7
Andy Ruz <86988806+Anghy73@users.noreply.github.com>
2024-02-03 00:17:57 -0300

    save message

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,12 @@
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
 import GoalsArea from './components/GoalsArea'
+import ProgressBar from './components/ProgressBar'
 
 function App () {
-  const [percentage, setPercentage] = useState(0)
-
-  const isYearLeap = (year) => {
-    if (year % 100 === 0 && year % 400 === 0) return true
-    if (year % 4 === 0 && year % 100 !== 0) return true
-    return false
-  }
-
-  const percentageOfYear = () => {
-    const date = new Date()
-    const year = date.getFullYear()
-    let totalDaysOfYear = 0
-    isYearLeap(year) ? totalDaysOfYear = 366 : totalDaysOfYear = 365
-
-    const currentDate = new Date(`01/01/${year}`)
-    const milliseconds = 24 * 60 * 60 * 1000
-    const elapsed = Math.abs(currentDate.getTime() - date.getTime())
-    const elapsedDays = Math.round(elapsed / milliseconds)
-
-    return Math.round(elapsedDays * 100 / totalDaysOfYear)
-  }
-
-  useEffect(() => {
-    if (percentage < percentageOfYear()) {
-      setTimeout(() => setPercentage(prev => (prev += 1)), 70)
-    }
-  }, [percentage])
-
   return (
     <main className='w-screen h-screen bg-gray-950 text-white px-5 selection:bg-indigo-950'>
       <GoalsArea />
-      <div className='absolute top-[50%] -translate-y-[50%] left-[50%] -translate-x-[50%] flex flex-col justify-center items-center w-full max-w-3xl lg:left-[50%] lg:-translate-x-[40%] p-5'>
-        <div className='sm:text-[13rem] text-8xl text-cyan-600 font-bold tracking-wide mb-20'>{`${percentage}`.padStart(3, '0')}%</div>
-        <div className='relative flex justify-start items-center border-2 border-indigo-600 rounded-lg w-full max-w-2xl h-14 p-2  bg-black'>
-          <div className='w-full'>
-            <div
-              className='bg-cyan-600 h-10 rounded' style={{
-                width: `${percentage}%`,
-                transition: 'width 1s'
-              }}
-            />
-          </div>
-          <div
-            className='absolute left-0 h-14 rounded-lg shadow-xl shadow-cyan-600 -z-10' style={{
-              width: `${percentage}%`,
-              transition: 'width 1s'
-            }}
-          />
-        </div>
-      </div>
+      <ProgressBar />
     </main>
   )
 }
--- src/components/GoalsArea.jsx
+++ src/components/GoalsArea.jsx
@@ -1,11 +1,25 @@
+import { useEffect } from 'react'
+
 function GoalsArea () {
-  const YEAR = new Date().getFullYear()
+  const year = new Date().getFullYear()
+
+  const handleChange = (e) => {
+    const message = e.target.value
+    // eslint-disable-next-line no-undef
+    localStorage.setItem('message', message)
+  }
+
+  useEffect(() => {
+    const goals = document.getElementById('goals')
+    // eslint-disable-next-line no-undef
+    goals.value = localStorage.getItem('message')
+  }, [])
 
   return (
-    <div className='border-2 border-indigo-600 rounded-xl overflow-hidden absolute top-[50%] translate-y-[-50%] left-5 sm:left-10 bg-[#0000008f] z-10'>
+    <div className='border-2 border-indigo-600 rounded-xl overflow-hidden absolute top-[50%] translate-y-[-50%] left-5 sm:left-10 bg-[#0000009f] z-10'>
       <label className='flex flex-col justify-center items-center'>
-        <p className='text-xl mb-5 mt-3 cursor-pointer text-amber-400'>Year <span>{YEAR}</span></p>
-        <textarea className='bg-transparent px-5 text-amber-400 focus-visible:outline-none min-h-96 max-h-[600px]' name='goals' id='' cols='30' rows='10' />
+        <p className='text-xl mb-5 mt-3 cursor-pointer text-amber-400'>Year <span>{year}</span></p>
+        <textarea onChange={handleChange} className='bg-transparent px-5 text-amber-400 focus-visible:outline-none min-h-96 max-h-[600px]' name='goals' id='goals' cols='30' rows='10' />
       </label>
     </div>
   )
